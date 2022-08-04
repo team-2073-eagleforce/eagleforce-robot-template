0e7522294eec29e4aa48bbb6ffb5ae0124131cb4
Ethan See <ethan_see@hotmail.com>
2022-08-04 09:01:02 -0700

    2022-offseason-update 1.2

--- src/main/java/com/team2073/robot/ApplicationContext.java
+++ src/main/java/com/team2073/robot/ApplicationContext.java
@@ -3,7 +3,7 @@ package com.team2073.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 
-import static com.team2073.robot.constants.AppConstants.*;
+import static com.team2073.robot.AppConstants.*;
 
 public class ApplicationContext {
 
